05b1159c0f1fa7c06eec94f8054e42e6a16a2bb4
Eirik Bjornset <ebjornset@gmail.com>
2022-06-03 20:34:44 +0200

    Synthetic change to verify that the build fails on git changes

--- src/DryGen/Options/CSharpFromJsonSchemaOptions.cs
+++ src/DryGen/Options/CSharpFromJsonSchemaOptions.cs
@@ -12,7 +12,7 @@ namespace DryGen.Options
         public string? Namespace { get; set; }
 
         [YamlMember(Alias = "array-type", ApplyNamingConventions = false)]
-        [Option("array-type", HelpText = "The generic array .NET type (default: 'ICollection').")]
+        [Option("array-type", HelpText = "The generic array .NET type (default: 'ICollection'). Synthetic change to verify that the build fails on git changes")]
         public string? ArrayType { get; set; }
 
         [YamlMember(Alias = "array-instance-type", ApplyNamingConventions = false)]
