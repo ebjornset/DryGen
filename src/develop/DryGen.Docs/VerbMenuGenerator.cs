b3b6cba31252743b1711641f2fa1e2351938436a
Eirik Bjornset <ebjornset@gmail.com>
2022-06-03 22:57:45 +0200

    Forgot a static keyword on a class with only static methods :-(

--- src/develop/DryGen.Docs/VerbMenuGenerator.cs
+++ src/develop/DryGen.Docs/VerbMenuGenerator.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DryGen.Docs
 {
-    public class VerbMenuGenerator
+    public static class VerbMenuGenerator
     {
         public static void GenerateVerbMenu(TextWriter writer)
         {
