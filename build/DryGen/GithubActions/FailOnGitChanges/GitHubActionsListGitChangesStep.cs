acde84b04b962027346013351248469c1a27d721
Eirik Bjornset <ebjornset@gmail.com>
2022-06-06 00:07:30 +0200

    Prepare v0.6.0-prerelease2 (take two)

---
+++ build/DryGen/GithubActions/FailOnGitChanges/GitHubActionsListGitChangesStep.cs
@@ -0,0 +1,18 @@
+﻿using Nuke.Common.CI.GitHubActions.Configuration;
+using Nuke.Common.Utilities;
+
+namespace DryGen.GithubActions.FailOnGitChanges
+{
+    public class GitHubActionsListGitChangesStep : GitHubActionsStep
+    {
+        public override void Write(CustomFileWriter writer)
+        {
+            writer.WriteLine($"- name: List any git changes");
+            using (writer.Indent())
+            {
+                writer.WriteLine("if: steps.get_changes.outputs.changed != 0");
+                writer.WriteLine("run: git status --porcelain");
+            }
+        }
+    }
+}
