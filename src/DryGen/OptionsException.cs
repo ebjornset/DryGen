f5c2baa8467ee7b0a541b66815438dfa37c6c12f
Eirik Bjornset <ebjornset@gmail.com>
2022-06-05 10:21:10 +0200

    Suppress a SQ bug about serializable exception, since we don't serialize this exception anywhere

--- src/DryGen/OptionsException.cs
+++ src/DryGen/OptionsException.cs
@@ -2,7 +2,9 @@
 
 namespace DryGen
 {
+    #pragma warning disable S3925 // "ISerializable" should be implemented correctly
     public class OptionsException : Exception
+    #pragma warning restore S3925 // "ISerializable" should be implemented correctly
     {
         public OptionsException(string message) : base(message) { }
     }
