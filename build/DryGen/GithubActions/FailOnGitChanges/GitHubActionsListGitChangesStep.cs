b99745d481ed901fce5a7acfcf85734eb8f5774f
Eirik Bjornset <ebjornset@gmail.com>
2022-06-05 22:07:57 +0200

    Debug why the example file is marked as changed by git

--- build/DryGen/GithubActions/FailOnGitChanges/GitHubActionsListGitChangesStep.cs
+++ build/DryGen/GithubActions/FailOnGitChanges/GitHubActionsListGitChangesStep.cs
@@ -14,6 +14,16 @@ namespace DryGen.GithubActions.FailOnGitChanges
                 writer.WriteLine("if: steps.get_changes.outputs.changed != 0");
                 writer.WriteLine("run: git status --porcelain");
             }
+            writer.WriteLine($"- name: Read file contents (for debuging of why a file has changed)");
+            using (writer.Indent())
+            {
+                writer.WriteLine("uses: andstor/file-reader-action@v1");
+                writer.WriteLine("with:");
+                using (writer.Indent())
+                {
+                    writer.WriteLine("path: \"docs/examples/Mermaid-Class-diagrams.md\"");
+                }
+            }
         }
     }
 }
