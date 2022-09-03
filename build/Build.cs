70d0ccc356070040ba4d93b61ac474b49311d942
Eirik Bjornset <ebjornset@gmail.com>
2022-09-03 16:24:50 +0200

    Trying nuke 6.1.2

--- build/Build.cs
+++ build/Build.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
@@ -20,7 +19,6 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 namespace DryGen.Build;
 
 [ExcludeFromCodeCoverage]
-[CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
 public partial class Build : NukeBuild
 {
