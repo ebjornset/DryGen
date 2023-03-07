219aade258e22d03e839ab571749768979a0311d
Eirik Bjornset <ebjornset@gmail.com>
2023-03-07 23:45:32 +0100

    Generating components with alias and label

--- src/DryGen.MermaidFromDotnetDepsJson/MermaidC4ComponentDiagramFromDotnetDepsJsonGenerator.cs
+++ src/DryGen.MermaidFromDotnetDepsJson/MermaidC4ComponentDiagramFromDotnetDepsJsonGenerator.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿using DryGen.Core;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DryGen.MermaidFromDotnetDepsJson;
@@ -8,7 +14,143 @@ public class MermaidC4ComponentDiagramFromDotnetDepsJsonGenerator
     public async Task<string> Generate(IMermaidC4ComponentDiagramFromDotnetDepsJsonOptions options)
     {
         await Task.CompletedTask;
+        var (targetJson, targetName) = await LoadValidTargetJson(options.InputFile);
+        var runtimeDependencyJsonList = LoadRuntimeDependencyJsonList(targetJson, targetName);
         var sb = new StringBuilder().AppendLine("C4Component");
+        foreach (var runtimeDependency in runtimeDependencyJsonList)
+        {
+            sb.Append("\tComponent(\"").Append(runtimeDependency.Name).Append("\", \"").Append(runtimeDependency.Name).AppendLine("\")");
+        }
         return sb.ToString();
     }
+
+    private IEnumerable<JProperty> LoadRuntimeDependencyJsonList(JObject targetJson, string targetName)
+    {
+        var targetProperties = targetJson.Properties();
+        if (targetProperties?.Any() != true)
+        {
+            return Array.Empty<JProperty>();
+        }
+        List<JProperty> runtimeDependencies = new List<JProperty>();
+        foreach (var targetProperty in targetProperties)
+        {
+            if (targetProperty.Count != 1)
+            {
+                throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'targets' has unexpected Count '{targetProperty.Count}'."));
+            }
+            var targetObjectFirst = targetProperty.First;
+            if (targetObjectFirst == null)
+            {
+                continue;
+            }
+            if (targetObjectFirst.Type != JTokenType.Object)
+            {
+                throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'targets' is of unexpected type '{targetObjectFirst?.Type}', expected '{nameof(JTokenType.Object)}'."));
+            }
+            if (!targetObjectFirst.Any())
+            {
+                continue;
+            }
+            var targetObjectFirstProperty = (JObject)targetObjectFirst;
+            var runtimeProperty = targetObjectFirstProperty.Property("runtime");
+            if (runtimeProperty == null)
+            {
+                continue;
+            }
+            if (runtimeProperty.Count != 1)
+            {
+                throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'runtime' has unexpected Count '{runtimeProperty.Count}'."));
+            }
+            var runtimeObjectFirst = runtimeProperty.First;
+            if (runtimeObjectFirst == null)
+            {
+                continue;
+            }
+            if (runtimeObjectFirst.Type != JTokenType.Object)
+            {
+                throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'runtime' is of unexpected type '{runtimeObjectFirst?.Type}', expected '{nameof(JTokenType.Object)}'."));
+            }
+            if (!runtimeObjectFirst.Any())
+            {
+                continue;
+            }
+            runtimeDependencies.Add(targetProperty);
+        }
+        if (!runtimeDependencies.Any())
+        {
+            throw new InvalidContentException(GetInvalidContentMessage($"Found no non empty runtime dependencies in target '{targetName}'."));
+        }
+        return runtimeDependencies;
+    }
+
+    private async Task<(JObject targetObject, string targetName)> LoadValidTargetJson(string? inputFile)
+    {
+        var depsJsonText = await File.ReadAllTextAsync(inputFile);
+        var depsJson = JObject.Parse(depsJsonText);
+        var targetsObject = GetTargetsObject(depsJson);
+        var (targetObject, targetName) = GetTargetsObjectPropertyObject(targetsObject);
+        return (targetObject, targetName);
+    }
+
+    private (JObject targetObject, string targetName) GetTargetsObjectPropertyObject(JObject targetsObject)
+    {
+        var targetsObjectProperties = targetsObject.Properties();
+        if (targetsObjectProperties?.Any() != true)
+        {
+            throw new InvalidContentException(GetInvalidContentMessage("'targets' is empty."));
+        }
+        JObject? targetObject = null;
+        string? targetName = null;
+        foreach (var targetsObjectProperty in targetsObjectProperties)
+        {
+            if (targetsObjectProperty.Count != 1)
+            {
+                throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'targets' has unexpected Count '{targetsObjectProperty.Count}'."));
+            }
+            var targetsObjectFirst = targetsObjectProperty.First;
+            if (targetsObjectFirst == null)
+            {
+                continue;
+            }
+            if (targetsObjectFirst.Type != JTokenType.Object)
+            {
+                throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'targets' is of unexpected type '{targetsObjectFirst?.Type}', expected '{nameof(JTokenType.Object)}'."));
+            }
+            if (!targetsObjectFirst.Any())
+            {
+                continue;
+            }
+            targetObject = (JObject)targetsObjectFirst;
+            targetName = targetsObjectProperty.Name;
+            break;
+        }
+        if (targetObject == null)
+        {
+            throw new InvalidContentException(GetInvalidContentMessage("Found no non empty 'target'."));
+        }
+        if (string.IsNullOrWhiteSpace(targetName))
+        {
+            throw new InvalidContentException(GetInvalidContentMessage("TODO: Is this possible?"));
+        }
+        return (targetObject, targetName);
+    }
+
+    private JObject GetTargetsObject(JObject depsJson)
+    {
+        JObject? targetsObject = null;
+        var targetsProperty = depsJson.Property("targets") ?? throw new InvalidContentException(GetInvalidContentMessage("'targets' is missing."));
+        if (targetsProperty.Count != 1)
+        {
+            throw new InvalidContentException(GetInvalidContentMessage($"TODO: 'targets' has unexpected Count '{targetsProperty.Count}', expected 1."));
+        }
+        var targetsFirst = targetsProperty.First;
+        if (targetsFirst?.Type != JTokenType.Object)
+        {
+            throw new InvalidContentException(GetInvalidContentMessage($"'targets' is of unexpected type '{targetsFirst?.Type}', expected '{nameof(JTokenType.Object)}'."));
+        }
+        targetsObject = (JObject)targetsFirst;
+        return targetsObject;
+    }
+
+    private string GetInvalidContentMessage(string message) => $"Invalid deps.json: {message}";
 }
--- src/develop/DryGen.UTests/Features/Mermaid/FromDotnetDepsJson/C4ComponentDiagram.feature
+++ src/develop/DryGen.UTests/Features/Mermaid/FromDotnetDepsJson/C4ComponentDiagram.feature
@@ -8,7 +8,17 @@ Scenario: Should generate Mermaid C4 Component diagram from .Net deps.json to co
 	Given this .Net depts json input file
 		"""
 		{
-			"targets": {}
+			"targets": {
+				".NETCoreApp,Version=v7.0": {
+				},
+				".NETCoreApp,Version=v7.0/win-x64": {
+					"TestAssembly/1.0.0": {
+						"runtime": {
+							"TestAssembly.dll": {}
+						}
+					}
+				}
+			}
 		}
 		"""
 	When I call the program with this command line arguments
@@ -18,5 +28,106 @@ Scenario: Should generate Mermaid C4 Component diagram from .Net deps.json to co
 	And console out should contain the text
 		"""
 		C4Component
+			Component("TestAssembly/1.0.0", "TestAssembly/1.0.0")
 		
 		"""
+
+Scenario: Mermaid C4 Component diagram generation should fail on missing targets
+	Given this .Net depts json input file
+		"""
+		{
+		}
+		"""
+	When I call the program with this command line arguments
+		| Arg                                               |
+		| mermaid-c4container-diagram-from-dotnet-deps-json |
+	Then I should get exit code '1'
+	And I should find the text "Invalid deps.json: 'targets' is missing." in console error
+
+Scenario: Mermaid C4 Component diagram generation should fail when targets is an array
+	Given this .Net depts json input file
+		"""
+		{
+			"targets": []
+		}
+		"""
+	When I call the program with this command line arguments
+		| Arg                                               |
+		| mermaid-c4container-diagram-from-dotnet-deps-json |
+	Then I should get exit code '1'
+	And I should find the text "Invalid deps.json: 'targets' is of unexpected type 'Array', expected 'Object'." in console error
+
+Scenario: Mermaid C4 Component diagram generation should fail on empty targets
+	Given this .Net depts json input file
+		"""
+		{
+			"targets": {}
+		}
+		"""
+	When I call the program with this command line arguments
+		| Arg                                               |
+		| mermaid-c4container-diagram-from-dotnet-deps-json |
+	Then I should get exit code '1'
+	And I should find the text "Invalid deps.json: 'targets' is empty." in console error
+
+Scenario: Mermaid C4 Component diagram generation should fail when targets only have empty properties
+	Given this .Net depts json input file
+		"""
+		{
+			"targets": {
+				".NETCoreApp,Version=v7.0": {
+				},
+				".NETCoreApp,Version=v7.0/win-x64": {
+				}
+			}
+		}
+		"""
+	When I call the program with this command line arguments
+		| Arg                                               |
+		| mermaid-c4container-diagram-from-dotnet-deps-json |
+	Then I should get exit code '1'
+	And I should find the text "Invalid deps.json: Found no non empty 'target'." in console error
+
+Scenario: Mermaid C4 Component diagram generation should fail when target only have assemblies without 'runtime' property
+	Given this .Net depts json input file
+		"""
+		{
+			"targets": {
+				".NETCoreApp,Version=v7.0": {
+					"TestAsembly/1.0.0": {
+					}
+				},
+				".NETCoreApp,Version=v7.0/win-x64": {
+				}
+			}
+		}
+		"""
+	When I call the program with this command line arguments
+		| Arg                                               |
+		| mermaid-c4container-diagram-from-dotnet-deps-json |
+	Then I should get exit code '1'
+	And I should find the text "Invalid deps.json: Found no non empty runtime dependencies in target '.NETCoreApp,Version=v7.0'." in console error
+
+Scenario: Mermaid C4 Component diagram generation should fail when target only have assemblies with empty 'runtime' property
+	Given this .Net depts json input file
+		"""
+		{
+			"targets": {
+				".NETCoreApp,Version=v7.0": {
+					"TestAsembly/1.0.0": {
+						"dependencies": {
+						},
+						"runtime": {
+						}
+					}
+				},
+				".NETCoreApp,Version=v7.0/win-x64": {
+				}
+			}
+		}
+		"""
+	When I call the program with this command line arguments
+		| Arg                                               |
+		| mermaid-c4container-diagram-from-dotnet-deps-json |
+	Then I should get exit code '1'
+	And I should find the text "Invalid deps.json: Found no non empty runtime dependencies in target '.NETCoreApp,Version=v7.0'." in console error
