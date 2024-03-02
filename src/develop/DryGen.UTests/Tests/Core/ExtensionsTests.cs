56f0649a78d5d0678fe6c241c776eae6baf23c08
Eirik Bjornset <ebjornset@gmail.com>
2024-03-02 19:32:25 +0100

    More test coverage

---
+++ src/develop/DryGen.UTests/Tests/Core/ExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using DryGen.Core;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DryGen.UTests.Tests.Core;
+
+public class ExtensionsTests
+{
+    [Fact]
+    public void AsNonNull_SHould_Throw_ArgumentNullException_For_Null_Vallue()
+    {
+        Action act = () => ((object?)null).AsNonNull();
+
+        act.Should().ThrowExactly<ArgumentNullException>();
+    }
+}
