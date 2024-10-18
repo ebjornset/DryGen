ded7401a947404ec85e1a8b53a64ef4168a328ef
Eirik Bjornset <ebjornset@gmail.com>
2024-10-18 21:19:10 +0200

    Trying to load asseblies from the Asp.Net Core and .Net runtime folders if its not present in the input folder (#124)

--- src/DryGen/InternalAssemblyLoadContext.cs
+++ src/DryGen/InternalAssemblyLoadContext.cs
@@ -6,44 +6,52 @@ namespace DryGen;
 
 internal class InternalAssemblyLoadContext : AssemblyLoadContext
 {
-    private readonly string inputFile;
-    private readonly bool useAssemblyLoadContextDefault;
-    private readonly string inputDirectory;
-    private AssemblyLoadContext AssemblyLoadContext => useAssemblyLoadContextDefault ? Default : this;
+	private readonly string inputFile;
+	private readonly bool useAssemblyLoadContextDefault;
+	private readonly string[] searchDirectories;
 
-    internal InternalAssemblyLoadContext(string inputFile, bool useAssemblyLoadContextDefault)
-    {
-        this.inputFile = inputFile;
-        this.useAssemblyLoadContextDefault = useAssemblyLoadContextDefault;
-        inputDirectory = Path.GetDirectoryName(inputFile) ?? throw new OptionsException($"Could not determine directory from inputFile '{inputFile}'");
-    }
+	private AssemblyLoadContext AssemblyLoadContext => useAssemblyLoadContextDefault ? Default : this;
 
-    internal Assembly Load()
-    {
-        // Seems like we cant put a using on the ContextualReflectionScope returned from EnterContextualReflection(),
-        // since the protected Load never gets called if we do and then we can't load any dependencies.
-        AssemblyLoadContext.EnterContextualReflection();
-        return LoadFromFile(inputFile);
-    }
+	internal InternalAssemblyLoadContext(string inputFile, bool useAssemblyLoadContextDefault)
+	{
+		this.inputFile = inputFile;
+		this.useAssemblyLoadContextDefault = useAssemblyLoadContextDefault;
+		var inputDirectory = Path.GetDirectoryName(inputFile) ?? throw new OptionsException($"Could not determine directory from inputFile '{inputFile}'");
+		var runtimeDirectory = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+		var aspNetCoreDirectoryApp = runtimeDirectory.Replace("Microsoft.NETCore.App", "Microsoft.AspNetCore.App");
+		var aspNetCoreDirectoryAll = runtimeDirectory.Replace("Microsoft.NETCore.App", "Microsoft.AspNetCore.All");
+		searchDirectories = new[] { inputDirectory, aspNetCoreDirectoryApp, aspNetCoreDirectoryAll, runtimeDirectory };
+	}
 
-    protected override Assembly? Load(AssemblyName assemblyName)
-    {
-        foreach (var extension in new[] { ".dll", ".exe" })
-        {
-            var assemblyFileName = $"{inputDirectory}{Path.DirectorySeparatorChar}{assemblyName.Name}{extension}";
-            if (File.Exists(assemblyFileName))
-            {
-                return LoadFromFile(assemblyFileName);
-            }
-        }
-        // We can't find the assembly file, let the runtime try to handle it
-        return null;
-    }
+	internal Assembly Load()
+	{
+		// Seems like we cant put a using on the ContextualReflectionScope returned from EnterContextualReflection(),
+		// since the protected Load never gets called if we do and then we can't load any dependencies.
+		AssemblyLoadContext.EnterContextualReflection();
+		return LoadFromFile(inputFile);
+	}
 
-    private Assembly LoadFromFile(string assemblyFileName)
-    {
-        // It seems like LoadFromAssemblyPath lockes the file, and that makes our teste fail, so we read the file manually to a stream
-        var assemblyBytes = File.ReadAllBytes(assemblyFileName);
-        return AssemblyLoadContext.LoadFromStream(new MemoryStream(assemblyBytes));
-    }
+	protected override Assembly? Load(AssemblyName assemblyName)
+	{
+		foreach (var extension in new[] { ".dll", ".exe" })
+		{
+			foreach(var loadDirectory in searchDirectories)
+			{
+				var assemblyFileName = $"{loadDirectory}{Path.DirectorySeparatorChar}{assemblyName.Name}{extension}";
+				if (File.Exists(assemblyFileName))
+				{
+					return LoadFromFile(assemblyFileName);
+				}
+			}
+		}
+		// We can't find the assembly file, let the runtime try to handle it
+		return null;
+	}
+
+	private Assembly LoadFromFile(string assemblyFileName)
+	{
+		// It seems like LoadFromAssemblyPath lockes the file, and that makes our teste fail, so we read the file manually to a stream
+		var assemblyBytes = File.ReadAllBytes(assemblyFileName);
+		return AssemblyLoadContext.LoadFromStream(new MemoryStream(assemblyBytes));
+	}
 }
