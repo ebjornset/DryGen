8c0a40775ed8541cc2da189c8303c57afde9d2f9
Eirik Bjornset <ebjornset@gmail.com>
2024-10-26 06:46:57 +0200

    Support for generating many to many relations in `mermaid-er-diagram-from-efcore` (#127)

---
+++ src/DryGen.MermaidFromEfCore/ModelRuntimeKey.cs
@@ -0,0 +1,20 @@
+﻿using DryGen.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DryGen.MermaidFromEfCore;
+
+public class ModelRuntimeKey : ModelElement
+{
+	private static Type? elementType;
+
+	public ModelRuntimeKey(object foreignKey) : base(foreignKey)
+	{
+		Properties = GetElementMandatoryPropertyValue<IReadOnlyList<object>>(nameof(Properties)).Select(x => new ModelProperty(x)).ToArray();
+	}
+
+	public IReadOnlyList<ModelProperty> Properties { get; }
+	protected override Type ElementType => elementType ??= RuntimeKeyTypeName.LoadTypeByName();
+}
