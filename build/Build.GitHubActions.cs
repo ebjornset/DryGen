cccbefbae2bb9b63e9353016ce9945cefd5abece
Eirik Bjornset <ebjornset@gmail.com>
2024-10-29 19:05:38 +0100

    Fix release permissions to deploy pages (#131)

--- build/Build.GitHubActions.cs
+++ build/Build.GitHubActions.cs
@@ -8,7 +8,7 @@ namespace DryGen.Build;
 [DotNetGitHubActions(name: "pr", OnPullRequestBranches = ["main"], InvokedTargets = [nameof(CiCd_Build)], PublishArtifacts = false, ImportSecrets = [nameof(SonarToken)])]
 [DotNetGitHubActions(name: "build", OnPushBranches = ["main"], OnWorkflowDispatchOptionalInputs = ["dummy"], InvokedTargets = [nameof(CiCd_Build)], PublishArtifacts = false, ImportSecrets = [nameof(SonarToken)])]
 [DotNetGitHubActions(name: "tag-version", AutoGenerate = false, OnWorkflowDispatchRequiredInputs = ["version"], InvokedTargets = [nameof(CiCd_TagVersion)], PublishArtifacts = false, ImportSecrets = [nameof(SonarToken)])]
-[NugetPushGitHubActions(name: "release", OnPushTags = ["v*.*.*"], InvokedTargets = [nameof(CiCd_Release)], PublishArtifacts = false, ImportSecrets = [nameof(NuGetApiKey), nameof(SonarToken)])]
+[NugetPushGitHubActions(name: "release", OnPushTags = ["v*.*.*"], InvokedTargets = [nameof(CiCd_Release)], PublishArtifacts = false, ImportSecrets = [nameof(NuGetApiKey), nameof(SonarToken)], WritePermissions = [GitHubActionsPermissions.IdToken, GitHubActionsPermissions.Pages], EnvironmentName = "github-pages")]
 [GhPagesGitHubActions(name: "publish-docs", On = [GitHubActionsTrigger.WorkflowDispatch], InvokedTargets = [nameof(CiCd_BuildDocs)], PublishArtifacts = false, WritePermissions = [GitHubActionsPermissions.IdToken, GitHubActionsPermissions.Pages], EnvironmentName = "github-pages")]
 public partial class Build
 {
